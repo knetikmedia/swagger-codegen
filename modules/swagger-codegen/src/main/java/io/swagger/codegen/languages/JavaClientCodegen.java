ff344bc87b6b6fd36fe317fd807e5a6d1234c4b5
Tyler Novotny <Tyler@knetik.com>
2017-08-25 14:58:59 -0400

    automatic oauth refresh token handling for Java client

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/JavaClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/JavaClientCodegen.java
@@ -160,6 +160,8 @@ public class JavaClientCodegen extends AbstractJavaCodegen
         supportingFiles.add(new SupportingFile("auth/ApiKeyAuth.mustache", authFolder, "ApiKeyAuth.java"));
         supportingFiles.add(new SupportingFile("auth/OAuth.mustache", authFolder, "OAuth.java"));
         supportingFiles.add(new SupportingFile("auth/OAuthFlow.mustache", authFolder, "OAuthFlow.java"));
+        supportingFiles.add(new SupportingFile("auth/OAuthExpirationHandler.mustache", authFolder, "OAuthExpirationHandler.java"));
+        supportingFiles.add(new SupportingFile("auth/OAuthRefreshExpirationHandler.mustache", authFolder, "OAuthRefreshExpirationHandler.java"));
         supportingFiles.add(new SupportingFile( "gradlew.mustache", "", "gradlew") );
         supportingFiles.add(new SupportingFile( "gradlew.bat.mustache", "", "gradlew.bat") );
         supportingFiles.add(new SupportingFile( "gradle-wrapper.properties.mustache",
