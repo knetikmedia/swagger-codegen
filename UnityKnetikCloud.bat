eac0e8063f3badf5030d1fb095db8abd0933c6cf
George Hornmoen <george.hornmoen@gmail.com>
2017-11-20 22:04:02 -0800

    Template files for SplytAnalytics.  The api.mustache file needs a small tweak to generate query parameters on the request URL properly for Unity REST - otherwise it incorrectly trys to url-encode the JSON body.

---
+++ UnityKnetikCloud.bat
@@ -0,0 +1,55 @@
+@ECHO OFF
+
+SET JSON_FILE=https://sandbox.knetikcloud.com/swagger-doc
+SET VERSION_NUMBER=3.0.7
+
+SET ID_FLAGS=--group-id com.knetikcloud --artifact-version %VERSION_NUMBER% -DprojectVersion=%VERSION_NUMBER%
+
+SET MODULES_FOLDER=%~dp0modules
+SET SDK_FOLDER=%~dp0sdk
+
+SET OUT_FOLDER=%SDK_FOLDER%\knetikcloud-unity
+SET UNITY_FOLDER=%OUT_FOLDER%\unity
+SET VENDOR_FOLDER=%OUT_FOLDER%\vendor
+
+SET UNITY_ASSETS_FOLDER=%UNITY_FOLDER%\Assets
+SET UNITY_PLUGIN_FOLDER=%UNITY_ASSETS_FOLDER%\Plugin
+SET UNITY_THIRD_PARTY_FOLDER=%UNITY_ASSETS_FOLDER%\ThirdParty
+SET UNITY_KNETIK_FOLDER=%UNITY_THIRD_PARTY_FOLDER%\KnetikCloud
+
+REM Clean up previous generations
+rd /S /Q "%OUT_FOLDER%\"
+
+REM Make folders
+mkdir "%SDK_FOLDER%\"
+mkdir "%OUT_FOLDER%\"
+
+REM Generate code
+java -jar "%MODULES_FOLDER%\swagger-codegen-cli\target\swagger-codegen-cli.jar" generate -i "%JSON_FILE%" -l CsharpUnity -DpackageName="com.knetikcloud" %ID_FLAGS% --artifact-id knetikcloud-csharp-unity-client -o "%OUT_FOLDER%"
+
+mkdir "%UNITY_FOLDER%\"
+mkdir "%UNITY_ASSETS_FOLDER%\"
+mkdir "%UNITY_PLUGIN_FOLDER%\"
+mkdir "%UNITY_THIRD_PARTY_FOLDER%\"
+mkdir "%UNITY_KNETIK_FOLDER%\"
+
+REM Copy code to the proper Unity layout
+XCOPY "%OUT_FOLDER%\src\main\CsharpUnity\com\knetikcloud\*" "%UNITY_KNETIK_FOLDER%\" /S /Y
+rd /S /Q "%OUT_FOLDER%\src\"
+
+REM Pull required plugins
+wget -nc https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+nuget.exe install "%VENDOR_FOLDER%\packages.config" -o "%VENDOR_FOLDER%"
+
+REM Copy plugins to the proper Unity folder
+XCOPY "%VENDOR_FOLDER%\RestSharp-Unity-NET-2.0.105.1\lib\net20\RestSharp.dll" "%UNITY_PLUGIN_FOLDER%\" /S /Y
+XCOPY "%VENDOR_FOLDER%\Unity.Newtonsoft.Json.7.0.0.0\lib\net35-Client\Unity.Newtonsoft.Json.dll" "%UNITY_PLUGIN_FOLDER%\" /S /Y
+rd /S /Q "%VENDOR_FOLDER%\"
+
+REM Remove unneccessary files
+del /F "%OUT_FOLDER%\compile-mono.sh"
+del /F "%UNITY_KNETIK_FOLDER%\Client\Configuration.cs"
+
+REM Rename the client classes to be more unique within the context of a Unity project
+rename "%UNITY_KNETIK_FOLDER%\Client\ApiClient.cs" KnetikClient.cs
+rename "%UNITY_KNETIK_FOLDER%\Client\ApiException.cs" KnetikException.cs
--- UnitySplytAnalytics.bat
+++ UnitySplytAnalytics.bat
@@ -1,21 +1,22 @@
 @ECHO OFF
 
-SET JSON_FILE=https://sandbox.knetikcloud.com/swagger-doc
+SET JSON_FILE=https://api.knetik.io/swagger-doc?group=apiv2
 SET VERSION_NUMBER=3.0.7
 
-SET ID_FLAGS=--group-id com.knetikcloud --artifact-version %VERSION_NUMBER% -DprojectVersion=%VERSION_NUMBER%
+SET ID_FLAGS=--group-id com.splytanalytics --artifact-version %VERSION_NUMBER% -DprojectVersion=%VERSION_NUMBER%
 
 SET MODULES_FOLDER=%~dp0modules
+SET UNITY_TEMPLATE_FOLDER=modules\swagger-codegen\src\main\resources\CsharpUnitySplyt
 SET SDK_FOLDER=%~dp0sdk
 
-SET OUT_FOLDER=%SDK_FOLDER%\csharp-unity
+SET OUT_FOLDER=%SDK_FOLDER%\splytanalytics-unity
 SET UNITY_FOLDER=%OUT_FOLDER%\unity
 SET VENDOR_FOLDER=%OUT_FOLDER%\vendor
 
 SET UNITY_ASSETS_FOLDER=%UNITY_FOLDER%\Assets
 SET UNITY_PLUGIN_FOLDER=%UNITY_ASSETS_FOLDER%\Plugin
 SET UNITY_THIRD_PARTY_FOLDER=%UNITY_ASSETS_FOLDER%\ThirdParty
-SET UNITY_KNETIK_FOLDER=%UNITY_THIRD_PARTY_FOLDER%\KnetikCloud
+SET UNITY_KNETIK_FOLDER=%UNITY_THIRD_PARTY_FOLDER%\SplytAnalytics
 
 REM Clean up previous generations
 rd /S /Q "%OUT_FOLDER%\"
@@ -25,7 +26,7 @@ mkdir "%SDK_FOLDER%\"
 mkdir "%OUT_FOLDER%\"
 
 REM Generate code
-java -jar "%MODULES_FOLDER%\swagger-codegen-cli\target\swagger-codegen-cli.jar" generate -i "%JSON_FILE%" -l CsharpUnity -DpackageName="com.knetikcloud" %ID_FLAGS% --artifact-id knetikcloud-csharp-unity-client -o "%OUT_FOLDER%"
+java -jar "%MODULES_FOLDER%\swagger-codegen-cli\target\swagger-codegen-cli.jar" generate -i "%JSON_FILE%" -l CsharpUnity -t "%UNITY_TEMPLATE_FOLDER%" -DpackageName="com.splytanalytics" %ID_FLAGS% --artifact-id splytanalytics-unity-client -o "%OUT_FOLDER%"
 
 mkdir "%UNITY_FOLDER%\"
 mkdir "%UNITY_ASSETS_FOLDER%\"
@@ -34,7 +35,7 @@ mkdir "%UNITY_THIRD_PARTY_FOLDER%\"
 mkdir "%UNITY_KNETIK_FOLDER%\"
 
 REM Copy code to the proper Unity layout
-XCOPY "%OUT_FOLDER%\src\main\CsharpUnity\com\knetikcloud\*" "%UNITY_KNETIK_FOLDER%\" /S /Y
+XCOPY "%OUT_FOLDER%\src\main\CsharpUnity\com\splytanalytics\*" "%UNITY_KNETIK_FOLDER%\" /S /Y
 rd /S /Q "%OUT_FOLDER%\src\"
 
 REM Pull required plugins
@@ -48,7 +49,7 @@ rd /S /Q "%VENDOR_FOLDER%\"
 
 REM Remove unneccessary files
 del /F "%UNITY_KNETIK_FOLDER%\Client\Configuration.cs"
+del /F "%UNITY_KNETIK_FOLDER%\Client\ApiException.cs"
 
 REM Rename the client classes to be more unique within the context of a Unity project
-rename "%UNITY_KNETIK_FOLDER%\Client\ApiClient.cs" KnetikClient.cs
-rename "%UNITY_KNETIK_FOLDER%\Client\ApiException.cs" KnetikException.cs
+rename "%UNITY_KNETIK_FOLDER%\Client\ApiClient.cs" SplytAnalyticsClient.cs
