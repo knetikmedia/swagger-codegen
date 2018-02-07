54daf02ddf39567818b3758184a849d1bcaedc63
George Hornmoen <george.hornmoen@gmail.com>
2018-02-06 23:21:32 -0800

    Updated the scripts for generating KnetikCloud and KnetikIO SDKs.  We could potentially merge the template files, but they are working as is.

--- UnityKnetikIO.bat
+++ UnityKnetikIO.bat
@@ -1,20 +1,20 @@
 @ECHO OFF
 
-SET JSON_FILE=https://sandbox.knetikcloud.com/swagger-doc
+SET JSON_FILE=https://api.knetik.io/swagger-doc?group=apiv2
 SET VERSION_NUMBER=3.0.7
 
-SET ID_FLAGS=--group-id com.knetikcloud --artifact-version %VERSION_NUMBER% -DprojectVersion=%VERSION_NUMBER%
+SET ID_FLAGS=--group-id com.knetikio --artifact-version %VERSION_NUMBER% -DprojectVersion=%VERSION_NUMBER%
 
 SET MODULES_FOLDER=%~dp0modules
-SET UNITY_TEMPLATE_FOLDER=modules\swagger-codegen\src\main\resources\CsharpUnity
+SET UNITY_TEMPLATE_FOLDER=modules\swagger-codegen\src\main\resources\CsharpUnityIO
 SET SDK_FOLDER=%~dp0sdk
 
-SET OUT_FOLDER=%SDK_FOLDER%\knetikcloud-unity
+SET OUT_FOLDER=%SDK_FOLDER%\knetikio-unity
 SET UNITY_FOLDER=%OUT_FOLDER%\unity
 
 SET UNITY_ASSETS_FOLDER=%UNITY_FOLDER%\Assets
 SET UNITY_THIRD_PARTY_FOLDER=%UNITY_ASSETS_FOLDER%\ThirdParty
-SET UNITY_KNETIK_FOLDER=%UNITY_THIRD_PARTY_FOLDER%\KnetikCloud
+SET UNITY_KNETIK_FOLDER=%UNITY_THIRD_PARTY_FOLDER%\KnetikIO
 
 REM Clean up previous generations
 rd /S /Q "%OUT_FOLDER%\"
@@ -24,7 +24,7 @@ mkdir "%SDK_FOLDER%\"
 mkdir "%OUT_FOLDER%\"
 
 REM Generate code
-java -jar "%MODULES_FOLDER%\swagger-codegen-cli\target\swagger-codegen-cli.jar" generate -i "%JSON_FILE%" -l CsharpUnity -t "%UNITY_TEMPLATE_FOLDER%" -DpackageName="com.knetikcloud" %ID_FLAGS% --artifact-id knetikcloud-csharp-unity-client -o "%OUT_FOLDER%"
+java -jar "%MODULES_FOLDER%\swagger-codegen-cli\target\swagger-codegen-cli.jar" generate -i "%JSON_FILE%" -l CsharpUnity -t "%UNITY_TEMPLATE_FOLDER%" -DpackageName="com.knetikio" %ID_FLAGS% --artifact-id knetikio-unity-client -o "%OUT_FOLDER%"
 
 mkdir "%UNITY_FOLDER%\"
 mkdir "%UNITY_ASSETS_FOLDER%\"
@@ -32,11 +32,10 @@ mkdir "%UNITY_THIRD_PARTY_FOLDER%\"
 mkdir "%UNITY_KNETIK_FOLDER%\"
 
 REM Copy code to the proper Unity layout
-XCOPY "%OUT_FOLDER%\src\main\CsharpUnity\com\knetikcloud\*" "%UNITY_KNETIK_FOLDER%\" /S /Y
+XCOPY "%OUT_FOLDER%\src\main\CsharpUnity\com\knetikio\*" "%UNITY_KNETIK_FOLDER%\" /S /Y
 rd /S /Q "%OUT_FOLDER%\src\"
 
 REM Remove unneccessary files
-del /F "%OUT_FOLDER%\compile-mono.sh"
 del /F "%UNITY_KNETIK_FOLDER%\Client\ApiClient.cs"
 del /F "%UNITY_KNETIK_FOLDER%\Client\ApiException.cs"
 del /F "%UNITY_KNETIK_FOLDER%\Client\Configuration.cs"
