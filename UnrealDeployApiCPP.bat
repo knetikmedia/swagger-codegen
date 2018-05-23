9b87a0f6ac092a4d5d24f53c456e28114d9e43c9
George Hornmoen <george.hornmoen@gmail.com>
2018-05-23 08:14:36 -0700

    Not required.

--- UnrealDeployApiCPP.bat
+++
@@ -1,7 +0,0 @@
-
-SET SDK_FOLDER=%~dp0sdk\knetikcloud-unreal\
-SET DEST_FOLDER=%~dp0..\knetikcloud-unreal-sdk\KnetikCloudSDK\Source\KnetikCloudSDK\api\
-
-cd "%SDK_FOLDER%api\"
-xcopy *.cpp "%DEST_FOLDER%" /U
-cd %~dp0
